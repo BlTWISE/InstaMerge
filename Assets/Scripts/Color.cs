9d6906b21300df50b31f197edeba2a185c6a037f
BlTWISE <52811202+BlTWISE@users.noreply.github.com>
2020-02-17 15:28:01 -0500

    Created color script

---
+++ Assets/Scripts/Color.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Color : MonoBehaviour
+{
+    //Create the list
+    public List<Material> Mats = new List<Material>();
+
+    void Awake()
+    {
+        //Change the colour randomly
+        GetComponent<Renderer>().material = Mats[Random.Range(0, Mats.Count)];
+    }
+}
