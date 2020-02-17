d92edc68e725e3f4dfb40058f3db2771e4ce3302
BlTWISE <52811202+BlTWISE@users.noreply.github.com>
2020-02-17 15:33:59 -0500

    Added Spawn script

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    //Declare variables
+    public GameObject Food;
+    public float Speed;
+
+    //Called when the game begins
+    void Start()
+    {
+        //Call the function over and over again
+        InvokeRepeating("Generate", 0, Speed);
+    }
+
+    //This function creates a new food bit
+    void Generate()
+    {
+        //Generate random x and y pixel positions
+        int x = Random.Range(0, Camera.main.pixelWidth);
+        int y = Random.Range(0, Camera.main.pixelHeight);
+
+        //Turn into word space
+        Vector3 Target = Camera.main.ScreenToWorldPoint(new Vector3(x, y, 0));
+        Target.z = 0;
+
+        //Create food object from prefab
+        Instantiate(Food, Target, Quaternion.identity);
+    }
+}
